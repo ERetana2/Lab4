4b071a1065cf7077c29fba3cefdbd427c7ff7e79
Efrain Retana <eretana2@hotmail.com>
2020-03-13 18:31:08 -0600

    Update:final exercise

--- Retana_Efrain_exercises_BTrees_3.py
+++ Retana_Efrain_exercises_BTrees_3.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar 11 09:52:23 2020
-
-@author: space
+@Author: Efrain Retana
+@Professor: Olac Fuentes
+@TA: Oscar Galindo
+@Assignment: Exercises BTrees 3
 """
 
 import btree
@@ -10,32 +11,36 @@ import matplotlib.pyplot as plt
 import math
 
 def largestAtDepthD(T,d):
-    if d > T.height():
+    # base case to check that d is not out of range
+    if d > T.height() or d < 0:
         return - math.inf
-    if type(T) == btree.BTree:
+    if type(T) == btree.BTree: # convert tree to iterable node
         T = T.root
-    if T.data == []:
+    if T.data == []: # if tree is empty
         return - math.inf
     temp = T
+    #iterate to the farthest right side of the tree until its leaf then return data[-1]
     while not temp.is_leaf and d != 0:
         temp = temp.child[-1]
         d -= 1
     return temp.data[-1]
 
 def findDepth(t,k): 
-    if type(t) == btree.BTree:
+    if type(t) == btree.BTree:  # make tree into iterable node
         t = t.root
-    #base cases
+    # if k in data return 0
     if k in t.data:
         return 0
+    # if k is not in the tree return -1
     if t.is_leaf:
         return -1
-    # increment
+    # increment until we find the branch that most be iterated to
     i = 0
     while i < len(t.data) and k > t.data[i]:
         i += 1
         
-    count = findDepth(t.child[i],k)
+    count = findDepth(t.child[i],k) # recursive call for child of tree to find k
+    # if it does not find k, return -1
     if count == -1:
         return -1
     return count  + 1
@@ -43,8 +48,9 @@ def findDepth(t,k):
 
     
 def printAtDepthD(t,d):
-    if type(t) == btree.BTree:
+    if type(t) == btree.BTree: #make tree into iterable node
         t = t.root
+        #iterate through each branch recursively until it finds depth d then print its node data
     for i in range(len(t.child)):
         if d != 0 and t.is_leaf:
             return
@@ -55,10 +61,10 @@ def printAtDepthD(t,d):
         printAtDepthD(t.child[i],d-1)
     
 def numLeaves(t):
-    if type(t) == btree.BTree:
+    if type(t) == btree.BTree: # iterable node
         t = t.root
-        
     count = 0
+    # if finds leaf return 1
     if t.is_leaf: # when t is a leaf return 1
         return 1
     # iterate through all the tree and count Nodes
@@ -67,46 +73,54 @@ def numLeaves(t):
     return count
 
 def fullNodesAtDepthD(t,d):
-    if type(t) == btree.BTree:
+    if type(t) == btree.BTree: # iterable node
         t = t.root
-    if t.is_leaf and d != 0:
+    if t.is_leaf and d != 0: # if d is out of range of tree height
         return 0
+    # if it finds depth d and node is full return 1
     if d == 0 and t.is_full():
         return 1
     count = 0
+    #iterate through each branch of the tree recursively
     for i in t.child:
         count += fullNodesAtDepthD(i,d-1)
     return count
-
+#---------------------------------
 def printDescending(t):
+    # print the list in reverse
     l = printDescending_n(t)
     for i in range(1,len(l)+1):
         print(l[-i],end= ' ')
         
-def printDescending_n(t):
-    if type(t) == btree.BTree:
+def printDescending_n(t): # wrapper function for printDescending
+    if type(t) == btree.BTree: #makes iterable node
         t = t.root
     tempL = []
+    # when at leaf, add the node data to a list and return the list
     if t.is_leaf:
         tempL.extend(t.data)
         return tempL
+    # iterate through each branch of the tree, but add the returned list to the current list
     for i in range(len(t.child)):
         tempL.extend(printDescending_n(t.child[i]))
+        # if i is less than the amount of childs - 1, then append current node data[i]
         if i != len(t.child) - 1:
             tempL.append(t.data[i])
     return tempL
-            
+#--------------------------------            
 
 def printItemsInNode(t,k):
-    if type(t) == btree.BTree:
+    if type(t) == btree.BTree:  # make tree into iterable node
         t = t.root
+    # if k is in current nodes data, print the data
     if k in t.data:
         for i in t.data:
             print(i,end = ' ')
         return
+    # if t is leaf and doest not contain k, return
     if t.is_leaf:
         return
-    # increment
+    # increment to desired child of tree, then recursively traverse that child[i]
     i = 0
     while i < len(t.data) and k > t.data[i]:
         i += 1
